05eb2bce6b1abdd3e52a3440c9dba3b6d1fdcf7f
Muhammad Firman Nur <muhammad.firman@bukalapak.com>
2021-07-21 06:08:12 +0700

    update package

--- Package.swift
+++ Package.swift
@@ -18,9 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AsyncDisplayKit"
-        ),
         .binaryTarget(
                     name: "AsyncDisplayKit",
                     url: "https://github.com/muhammadfirman/AsyncDisplayKit/releases/download/3.0.0/AsyncDisplayKit.xcframework.zip",
